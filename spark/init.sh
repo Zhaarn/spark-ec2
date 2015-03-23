9aa93d2bd094afcb8d0aeffc1359b6600a7442b2
Zhaarn Maheswaran <Zhaarn@gmail.com>
2015-03-23 16:33:39 -0400

    Add scala 2.11 option

--- spark/init.sh
+++ spark/init.sh
@@ -18,7 +18,7 @@ then
   git remote add origin $repo
   git fetch origin
   git checkout $git_hash
-  sbt/sbt clean assembly
+  sbt/sbt -Dscala-2.11="1" clean assembly
   sbt/sbt publish-local
   popd > /dev/null
 
